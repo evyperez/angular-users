12c374040054cad6a854145ee5d634343d6d0a2a
Evelyn Perez <evelynpinho@gmail.com>
2022-06-27 19:02:41 -0300

    feat: import loading on app module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoadingService } from './shared/preloader/loading.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-search';
+
+  constructor(
+    public loadingService: LoadingService,
+) { }
 }
+
